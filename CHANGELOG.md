3f7561d07f87b7e5bf1496ef84d646ed1669b1eb
Spencer Owen <owenspencer@gmail.com>
2015-12-01 08:54:25 -0700

    1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 0.1.3
+Fixes bug where wouldn't restart service
+
 # 0.1.2
 Manages init script to work around issue https://github.com/yahoo/kafka-manager/issues/13#issuecomment-160996849
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'owenspencer@gmail.com'
 license 'MIT'
 description 'Installs/Configures kafka-manager'
 long_description 'Installs/Configures kafka-manager'
-version '0.1.2'
+version '0.1.3'
 
 depends 'apt'
 depends 'packagecloud'
