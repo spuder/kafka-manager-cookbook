a418610a40c9a08f153840f9a27ccb4d1118e11c
Spencer Owen <owenspencer@gmail.com>
2015-11-30 20:44:39 -0700

    Adds changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.1.1
+Updates metadata for supermarket
+
+# 0.1.0
+Initial Release
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'owenspencer@gmail.com'
 license 'MIT'
 description 'Installs/Configures kafka-manager'
 long_description 'Installs/Configures kafka-manager'
-version '0.1.0'
+version '0.1.1'
 
 depends 'apt'
 depends 'packagecloud'
